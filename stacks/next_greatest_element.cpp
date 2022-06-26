ba7cb40ebb2c34b95603a74a601b6e0429c9aa9b
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-06-26 19:10:48 +0530

    medium

--- stacks/next_greatest_element.cpp
+++ stacks/next_greatest_element.cpp
@@ -1,9 +1,3 @@
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
- // } Driver Code Ends
 class Solution
 {
     public:
@@ -27,26 +21,3 @@ class Solution
         return nge;
     }
 };
-
-// { Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        
-        int n;
-        cin>>n;
-        vector<long long> arr(n);
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        
-        Solution obj;
-        vector <long long> res = obj.nextLargerElement(arr, n);
-        for (long long i : res) cout << i << " ";
-        cout<<endl;
-    }
-	return 0;
-}  // } Driver Code Ends
